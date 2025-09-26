2c193c4f5d29936afbd3cac83df36d36555d8283
chiblim <mehdi.chibli@grenoble-inp.org>
2025-09-26 05:25:53 +0200

    main.py

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from interface_stec import lancer_interface
 def main():
     print("=== STEC – Lancement de l'interface ===")
     lancer_interface()
-    print("\n✅ Programme terminé. Consultez le dossier 'OUTPUT/' pour les résultats.")
+    print("\n Programme terminé. Consultez le dossier 'OUTPUT/' pour les résultats.")
 
 if __name__ == "__main__":
     main()
+
